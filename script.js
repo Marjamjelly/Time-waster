1564c873d9459c073a13268258fd15138e05ee4d
Marjamjelly <130711938+Marjamjelly@users.noreply.github.com>
2025-04-10 20:00:29 -0500

    save

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-var seconds = 0;
+let seconds = 0;
 
-function timeWaste(number){
-    seconds = seconds + number;
-    document.getElementById("seconds").innerHTML = seconds;
-};
-window.setInterval(function(){	
-	timeWaste(1);
-}, 1000);
+function incrementCounter() {
+    counter++;
+    document.getElementById("time").textContent = seconds;
+  }
 
-var i = 0;
-
-var interval = setInterval( increment, 1000);
-
-function increment(){
-    i = i + 1;
-    document.getElementById("seconds").innerHTML = i;
-}
+  const intervalId = setInterval(incrementCounter, 1000);
