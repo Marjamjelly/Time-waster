5be5e0183cbf3b1b7720c71ab1f80ad75b46ad9a
Marjamjelly <130711938+Marjamjelly@users.noreply.github.com>
2025-04-10 21:18:27 -0500

    save

--- script.js
+++
@@ -1,8 +0,0 @@
-let seconds = 0;
-
-function incrementCounter() {
-    counter++;
-    document.getElementById("time").textContent = seconds;
-  }
-
-  const intervalId = setInterval(incrementCounter, 1000);
